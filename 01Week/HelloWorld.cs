3ba003a9f9165cc140c0028bc74b80e3eb521cce
Kristian Houghtling <firenze_vr@protonmail.com>
2018-01-20 19:56:37 -0600

    Hello World Test2

--- 01Week/HelloWorld.cs
+++ 01Week/HelloWorld.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine(value: "Hello World Test");
+        Console.WriteLine("Hello World!");
         Console.ReadLine();
     }
 }
